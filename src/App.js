0f66677e6e514fa89667c613460da5db66158d4d
Fabricio Polo <fabricio.polo@gmail.com>
2019-08-13 12:41:29 -0300

    list users component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
 import React, { Component } from "react";
+import Navbar from "./components/layout/Navbar";
+import Users from "./components/users/Users";
+
+import axios from "axios";
+
 import "./App.css";
 
 class App extends Component {
+  state = {
+    users: [],
+    loading: false
+  };
+
+  async componentDidMount() {
+    this.setState({ loading: true });
+
+    const response = await axios.get(
+      `https://api.github.com/users?client_id=${
+        process.env.REACT_APP_GITHUB_CLIENT_ID
+      }&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}}`
+    );
+
+    this.setState({ users: response.data, loading: false });
+  }
+
   render() {
     return (
       <div className='app'>
-        <h1>Hello React</h1>
+        <Navbar />
+        <div className='container'>
+          <Users loading={this.state.loading} users={this.state.users} />
+        </div>
       </div>
     );
   }
