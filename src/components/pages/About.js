2cce9638d53c5955ac3c5b2873f4b50d5650c613
Fabricio Polo <fabricio.polo@gmail.com>
2019-08-19 09:09:42 -0300

    add home e notfound

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -6,6 +6,12 @@ const About = () => {
       <h1>Sobre este App</h1>
       <p>Procura usuários do GitHub</p>
       <p>Versão: 1.0.0</p>
+      <br />
+      <p>Aplicação feita para estudar conceitos de ReactJS.</p>
+      <p>
+        Link para <a href='http://fapolo.github.io'>Meu portfolio particular</a>
+        .
+      </p>
     </Fragment>
   );
 };
---
+++ src/components/pages/NotFound.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const NotFound = () => {
+  return (
+    <div>
+      <h1>Página Não Encontrada</h1>
+      <p className='lead'>A página que você procura não existe...</p>
+    </div>
+  );
+};
+
+export default NotFound;
